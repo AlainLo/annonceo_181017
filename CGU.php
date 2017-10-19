decfee60816657147b3ee608b8cac7efebcd228c
Alain LORTAL <alain.lortal@lepoles.com>
2017-10-19 14:06:23 +0200

    Commit 4
    le légal (sous formes de 2 pages PHP non stylisées)

---
+++ CGU.php
@@ -0,0 +1,53 @@
+1. Définitions
+
+Chacun des termes mentionnés ci-dessous aura dans les présentes Conditions Générales de Vente du Service Annonceo ( ci-après dénommées les « CGV » ) la signification suivante :
+
+Annonce : désigne l'ensemble des éléments et données (visuelles, textuelles, sonores, photographies, dessins), déposé par un Membre sous sa responsabilité éditoriale exclusive, en vue d'acheter, de louer ou de vendre un bien ou un service et diffusé sur le Site Internet et les Applications. 
+
+Membre : désigne toute personne physique, majeure, agissant exclusivement à des fins privées et non professionnelles (tout Membre professionnel est dans l'obligation d'ouvrir un Compte Pro et de respecter les CGV applicables aux détenteurs d'un « Compte Pro »), établie en France (inclus les DOM à l'exclusion de Mayotte), titulaire d’un Compte Personnel et ayant déposé une Annonce, à partir de celui-ci, sur le Site Internet, et/ou sur les Applications. Tout Membre doit impérativement être connecté à son Compte Personnel pour déposer et gérer sa ou ses Annonces. Tout premier dépôt d’annonce entraîne automatiquement la création d’un Compte Personnel propre à l’Annonceur. 
+
+Applications : désigne l’application Android téléchargeable gratuitement sur un téléphone Android depuis le Google Play Store, et l’application optimisée pour iPhone et iPad téléchargeable depuis l’App Store d’Apple gratuitement sur un iPhone ou un iPad disposant a minima, de la version iOS 7 pour iPhone, ou de la version iOS 8 pour iPad, permettant aux Utilisateurs et aux Annonceurs d’accéder au Service leboncoin tel que défini à l’article 3.1 des CGU accessible ici.
+
+Compte Personnel : désigne l'espace gratuit que tout Membre doit se créer et auquel il doit se connecter depuis le Site Internet et les Applications, afin  de diffuser, gérer, visualiser ses annonces, souscrire à une ou des option(s) payante(s) et acheter des crédits.
+
+Annonceo : désigne la société qui édite et exploite le Site Internet et les Applications Annonceo, SAS au capital de 0,5 euros, immatriculée au registre du commerce et des sociétés de Paris sous le numéro 521.724.336 dont le siège social est situé 11 Avenue Saint-Exupéry, 92 Villeneuve-La-Garenne. 
+
+Service client : désigne le service de Annonceo auprès duquel l'Membre peut obtenir toute information complémentaire. Ce service peut être contacté par e-mail en cliquant sur le lien présent sur le Site Internet et les Applications. 
+
+Service Annonceo : désigne les services Annonceo mis à la disposition des Utilisateurs et des Annonceurs sur le Site Internet et les Applications tels que décrits à l'article 3.1 des CGU accessible ici.
+
+Site Internet : désigne le site internet exploité par Annonceo accessible principalement depuis l'URL www.annonceo.fr et permettant aux Utilisateurs et aux Annonceurs d'accéder via internet au Service leboncoin tel que décrit à l'article 3.1 des CGU accessible ici.
+
+Utilisateur : désigne tout visiteur, ayant accès au Service leboncoin via le Site Internet et/ou les Applications et consultant le Service leboncoin accessible depuis les différents supports. 
+
+2. Objet
+Les présentes Conditions Générales de Vente (CGV) établissent les conditions contractuelles applicables à toute souscription, par un Membre connecté à son Compte Personnel, d'option(s) payante(s) et d’achat de crédits depuis le Site Internet et les Applications. 
+
+3. Paiement et Facturation
+
+
+3.1 Remboursement
+Si la diffusion d'une Annonce est refusée par Annoncéo en raison de sa non-conformité aux règles de diffusion du Service leboncoin : 
+
+le montant de la ou des option(s) payante(s) souscrite(s) lors du dépôt ou de la modification de l'Annonce ne sera pas prélevé et un email sera adressé à le Membre l'informant que son Annonce a été refusée par Annoncéo et ne sera pas diffusée ;
+les crédits utilisés pour payer le montant de la ou des option(s) payante(s) ne seront pas prélevés du Compte Personnel de le Membre ;
+dans le cas d'un paiement de la ou des option(s) payante(s) par crédits et carte bancaire, les sommes payées seront restituées sous forme de crédits, ce que l'Membre reconnait et accepte.
+En revanche, si Annoncéo était contraint de supprimer l'Annonce en cours de diffusion en raison de sa non-conformité aux règles de diffusion du Service leboncoin et notamment parce que son contenu a été, légitimement, signalé comme abusif, Annoncéo ne remboursera pas à le Membre les crédits et/ou le montant de la ou des option(s) payante(s) souscrite(s) lors du dépôt ou de la modification de l'Annonce ainsi supprimée en cours de diffusion.
+
+En cas de suppression d'un Compte Personnel
+
+La suppression d'un Compte Personnel entraine la suppression de toute(s) Annonce(s) attachée(s), et en conséquence, la suppression de toute(s) option(s) payante(s) souscrite(s) pour ces Annonces et ne donne droit à aucun droit à remboursement, ni indemnité, ce que le Membre reconnait et accepte.
+
+La suppression d'un Compte Personnel entraine la suppression des crédits associés, n'ouvrant aucun droit à remboursement, ce que le Membre reconnait et accepte.
+
+3.2 Rétractation
+
+En application des articles L 221-18 et suivants du code de la consommation, le Membre dispose d'un délai de quatorze (14) jours francs pour se rétracter. Toutefois, en validant la commande, le Membre accepte que Annoncéo commence à exécuter la prestation et renonce expressément à son droit de rétractation. 
+
+4. La responsabilité de LBC FRANCE ne peut être engagée en cas d'inexécution ou de mauvaise exécution de la commande due, soit du fait de l'Annonceur, soit d'un cas de force majeure.
+
+5. Les présentes CGU sont applicables à partir du 08 février 2017.
+
+Annoncéo se réserve la possibilité, à tout moment, de modifier en tout ou partie les CGU.
+
+Les Membres sont invités à consulter régulièrement les CGU afin de prendre connaissance des changements apportés.
---
+++ mentions_legales.php
@@ -0,0 +1,8 @@
+Mentions légales
+
+Directeur de la Publication : Gregory
+raison sociale : Société Annonceo
+
+Adresse : 300 Boulevard de Vaugirard, 75015 Paris, France
+
+contact@annonceo.fr
